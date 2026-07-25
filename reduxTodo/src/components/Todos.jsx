c1d7cf21f89e49df01625834d65173dfc682954c
Arjun Prajapati <arjunprajapat2002211@gmail.com>
2026-07-25 13:32:22 +0530

    made the edit functionality in reduxTodo

--- reduxTodo/src/components/Todos.jsx
+++ reduxTodo/src/components/Todos.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { removeTodo } from '../features/todo/todoSlice';
+import { removeTodo, editTodo } from '../features/todo/todoSlice';
 
 function Todos() {
+    const [isEditable, setIsEditable] = useState(false);
+    const [editableId, setEditableId] = useState(null);
     const todos = useSelector((state => state.todos));
+    const [todoText, setTodoText] = useState()
     const dispatch = useDispatch();
+
+    const handleEdit = (todo) => {
+        setIsEditable(!isEditable);
+        setEditableId(todo.id);
+        setTodoText(todo.text);
+
+        if (isEditable) {
+            dispatch(editTodo({
+                id: todo.id,
+                text: todoText,
+            }))
+        }
+    }
+
+    // useEffect(() => {
+
+    // }, [todoText])
 
     return (
         <>
             <ul className="list-none mt-4">
                 {todos.map((todo) => (
                     <li
-                        className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+                        className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 text-whhite rounded"
                         key={todo.id}
                     >
-                        <div className='text-white'>{todo.text}</div>
+                        <input type="text" name="" id="" value={editableId === todo.id ? todoText : todo.text} onChange={(e) => setTodoText(e.target.value)} readOnly={editableId !== todo.id} className='text-white' />
+                        <button
+                            onClick={() => { handleEdit(todo); }}
+                            className="text-white bg-red-500 border-0 py-1 px-3 focus:outline-none hover:bg-red-600 rounded text-md"
+                        >{editableId === todo.id ? "Save" : "Edit"}</button>
                         <button
                             onClick={() => dispatch(removeTodo(todo.id))}
                             className="text-white bg-red-500 border-0 py-1 px-3 focus:outline-none hover:bg-red-600 rounded text-md"
--- reduxTodo/src/features/todo/todoSlice.js
+++ reduxTodo/src/features/todo/todoSlice.js
@@ -17,14 +17,15 @@ export const todoSlice = createSlice({
 
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !==  action.payload);
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
             console.log(state.todos);
         },
-
+        editTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => todo.id === action.payload.id ? {...todo, text : action.payload.text} : todo);
+        }
     }
 });
 
-export const {addTodo, removeTodo} =  todoSlice.actions; 
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
- 
